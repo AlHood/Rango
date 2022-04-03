7d9cd1492045273c3d4b9a18c26a4433f5a55fe7
AlHood <alastair.hood85@gmail.com>
2022-04-03 14:42:33 +0100

    Chap 7 exercises bugfixing

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
+    path('/category/{{ category.slug }}/add_page//', views.add_page, name='add_page'),
+    #re above, the overall URL should be /rango/category/{{ category.slug }}/add_page/, but surely the project
+    #handles the first /rango/ ?
+
 ]
 
