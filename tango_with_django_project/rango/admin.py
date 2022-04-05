76c07916ac17b7112a17994f2d7f56e3b3cc81cc
AlHood <alastair.hood85@gmail.com>
2022-04-05 19:20:43 +0100

    Chapter 9.5

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,9 +1,11 @@
 # Register your models here.
 from django.contrib import admin
 from rango.models import Category, Page, PageAdmin, CategoryAdmin
+from rango.models import UserProfile
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
 # Add in this class to customise the Admin Interface
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib import admin
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 
@@ -40,3 +42,14 @@ class CategoryAdmin(admin.ModelAdmin):
 #    Finally, register the PageAdmin class with Django’s admin interface.
 #You should modify the line admin.site.register(Page). Change it to
 #admin.site.register(Page, PageAdmin) in Rango’s admin.py file
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
