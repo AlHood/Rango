48e1663308780a7522fde455e1baf910e605f616
AlHood <alastair.hood85@gmail.com>
2022-04-05 20:19:07 +0100

    Chapter 9.6

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,14 +3,21 @@ from rango import views
 app_name = 'rango'
 
 
+#urlpatterns = [
+    #path('', views.index, name='index'),
+    #path('about/', views.about, name='about'),
+    #path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    #path('add_category/', views.add_category, name='add_category'),
+    #path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
+    #re above, the overall URL should be /rango/category/<slug:category_name_slug>/add_page/, but surely the project
+    #handles the first /rango/ ?
+#]
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
-    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
-    #re above, the overall URL should be /rango/category/<slug:category_name_slug>/add_page/, but surely the project
-    #handles the first /rango/ ?
-
+    path('register/', views.register, name='register'), # New mapping!
 ]
-
